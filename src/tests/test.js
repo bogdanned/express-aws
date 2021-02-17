bb222fcecf96706080ec7de83ea5d5cacb5aaa7d
Bogdan Nedelcu <bogdanned32@gmail.com>
2021-02-17 15:53:48 +0100

    app restructured

--- src/tests/test.js
+++ src/tests/test.js
@@ -1,16 +1,13 @@
-'use strict';
-
 const supertest = require('supertest'); 
-const test = require('unit.js');
 const app = require('../app.js');
 
-const request = supertest(app);
+
 
 describe('Tests app', function() {
+  const request = supertest(app);
   it('verifies get', function(done) {
     request.get('/').expect(200).end(function(err, result) {
-      test.string(result.text).contains('CI / CD Test');
-      test.value(result).hasHeader('content-type', 'text/html; charset=UTF-8');
+      expect(result.body).toHaveProperty("test","hello world!" );
       done(err);
     });
   });
