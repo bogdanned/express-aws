90f2e43a611eee6dff01b12aff248cd355b116a1
Bogdan Nedelcu <bogdanned32@gmail.com>
2021-02-18 08:11:46 +0100

    added extra response params

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,6 @@ import express, {Response, Request}  from 'express';
 
 const app = express();
 
-app.get("/", (req: Request,res: Response) => (res.json({test: "hello world!"})))
+app.get("/", (req: Request,res: Response) => (res.json({test: "hello world!", code: 5000})))
 
 export default app;
